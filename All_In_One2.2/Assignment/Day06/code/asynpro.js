af96d863b09bfa2f11c846a6d14e5bfcafa7dfeb
MrManoj124 <manojmanorooban@gmail.com>
2026-08-02 13:24:37 +0530

    Delete Assignment folder

--- All_In_One2.2/Assignment/Day06/code/asynpro.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-fs.readFile('file.txt','utf8',(err,data)=>{
-    if(err){
-        console.error(err);
-    }
-    console.log(data)
-});
-
-console.log("File reading is done...")
