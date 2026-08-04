f7870617f4dd6865a416cd2aec56e14c8402a354
MrManoj124 <manojmanorooban@gmail.com>
2026-08-04 14:26:57 +0530

    feat: refactor authentication context and remove unused Login component

--- BasicLogin_Auth/client/src/components/Login.jsx
+++
@@ -1,6 +0,0 @@
-import { useState, useContext } from "react";
-import {useNavigate} from 'react-router-dom';
-import {AuthContext} from 
-import {loginUser, googleLogin, FacebookLogin} from 
-import {validateEmail} from 
-import './Login.css';
