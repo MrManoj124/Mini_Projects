9b72200a7d192521db9427d35e51867d9448196a
MrManoj124 <manojmanorooban@gmail.com>
2026-08-04 21:23:22 +0530

    feat: implement form validation logic for email and password fields

--- BasicLogin_Auth/client/src/components/Login.jsx
+++ BasicLogin_Auth/client/src/components/Login.jsx
@@ -23,7 +23,29 @@ export default function Login(){
         }
     };
 
-    
+    const validateForm = () => {
+        const newErrors = {};
+
+        if(!formData.email) {
+            newErrors.email = 'Email is required';
+        }
+        else if(!validateEmail(formData.email)){
+            newErrors.email = 'Invalid email format';
+        }
+
+        if(!formData.password){
+            newErrors.password = 'Password is required';
+        }
+        else if(formData.password.length < 6){
+            newErrors.password = 'Password must be atleast 6 characters';
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
 
     
 
