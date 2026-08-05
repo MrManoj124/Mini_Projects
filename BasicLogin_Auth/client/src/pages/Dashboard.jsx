0970ea60742177592f93cf6df6b5c4b2f33a7a1f
MrManoj124 <manojmanorooban@gmail.com>
2026-08-05 22:54:30 +0530

    feat: implement Dashboard component with logout functionality

--- BasicLogin_Auth/client/src/pages/Dashboard.jsx
+++ BasicLogin_Auth/client/src/pages/Dashboard.jsx
@@ -3,3 +3,14 @@ import {useNavigate} from 'react-route-dom';
 import {AuthContext} from '../context/AuthContext';
 import './Dashboard.css';
 
+export default function Dashboard(){
+    const navigate = useState();
+    const {user, logout} = useContext(AuthContext);
+
+    const handleLogout  = () => {
+        logout();
+        navigate('/login');
+    };
+
+    
+}
