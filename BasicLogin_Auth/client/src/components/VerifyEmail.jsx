b780a9f58243ed59a3ad4e1071b329cb8a7ab554
MrManoj124 <manojmanorooban@gmail.com>
2026-08-05 23:56:05 +0530

    feat: add useState and useEffect imports to VerifyEmail component

--- BasicLogin_Auth/client/src/components/VerifyEmail.jsx
+++ BasicLogin_Auth/client/src/components/VerifyEmail.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import {useNavigate , useSearchParams} from 'react-router-dom';
 import {verifyEmail, sendVerificationEmail} from '../services/api';
 import './VerifyEmail.css';
