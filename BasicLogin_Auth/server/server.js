3d03baa278d669252e8da6b4113aacb0ea1d61cf
MrManoj124 <manojmanorooban@gmail.com>
2026-08-25 22:32:38 +0530

    feat: Initialize server with essential middleware and auth routes

---
+++ BasicLogin_Auth/server/server.js
@@ -0,0 +1,6 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.js";
+// import userRoutes from "./routes/user.js";
