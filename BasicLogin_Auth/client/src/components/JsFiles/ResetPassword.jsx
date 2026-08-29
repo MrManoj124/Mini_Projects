7d3df1e75fc96bffeb915e8e33c5acf7d828f4ea
MrManoj124 <manojmanorooban@gmail.com>
2026-08-29 23:33:29 +0530

    feat: Remove resetPassword function implementation from ResetPassword component

--- BasicLogin_Auth/client/src/components/JsFiles/ResetPassword.jsx
+++ BasicLogin_Auth/client/src/components/JsFiles/ResetPassword.jsx
@@ -5,44 +5,5 @@ import {checkPasswordStrength, passwordsMatch} from '../../utils/validation';
 import '../CssFiles/ResetPassword.css';
 
 
-export const resetPassword = async(req,res) => {
-    try{
-        const{token, password}=req.body;
-        if(!isValidPassword(password)){
-            return res.status(400).json({
-                success : false,
-                message : 'Password must be at least 6 characters'
-            });
-        }
 
-        const user =  await User.findOne({
-            resetPasswordToken : token,
-            resetPasswordExpires : {$gt:Date.now()}
-        });
 
-        if(!user) {
-            return res.status(400).json({
-                success : false,
-                message : 'Invalid or expired reset token'
-            });
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-        user.resetPasswordToken = undefined;
-        user.reserPasswordExpires = undefined;
-        await user.save();
-
-        res.json({
-            success : true,
-            message : 'Password reset successful. You can now login with your new password.'
-        });
-    }
-    catch(error){
-        console.error('Reset password error:', error);
-        res.status(500).json({
-            success:false,
-            message:'Server error'
-        });
-    }
-};
