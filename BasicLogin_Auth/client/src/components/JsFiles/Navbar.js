039d813802d6213dc6402c66f81bfebac05dbf9b
MrManoj124 <manojmanorooban@gmail.com>
2026-09-01 23:20:33 +0530

    feat: Add Navbar component with styling and authentication context

---
+++ BasicLogin_Auth/client/src/components/JsFiles/Navbar.js
@@ -0,0 +1,5 @@
+import {useContext} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {AuthContext} from '../context/AuthContext';
+import '../CssFiles/Navbar.css'; // Import the CSS file for styling
+
