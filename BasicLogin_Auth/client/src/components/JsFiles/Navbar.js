ce77c17bc9d895252370418e3c2e4de82ffd007d
MrManoj124 <manojmanorooban@gmail.com>
2026-09-01 23:39:38 +0530

    feat: Implement Navbar component with user authentication and navigation

--- BasicLogin_Auth/client/src/components/JsFiles/Navbar.js
+++ BasicLogin_Auth/client/src/components/JsFiles/Navbar.js
@@ -3,3 +3,32 @@ import {useNavigate} from 'react-router-dom';
 import {AuthContext} from '../context/AuthContext';
 import '../CssFiles/Navbar.css'; // Import the CSS file for styling
 
+export default function Navbar(){
+    const navigate = useNavigate();
+    const {user, logout} =useContext(AuthContext);
+
+    const handleLogout = () => {
+        Logout();
+        navigate('/login');
+    };
+
+    return(
+        <nav className='navbar'>
+            <div className='nav-conatainer'>
+                <div className='nav-logo'>
+                    <h2>MyApp</h2>
+                </div>
+                <div className='nav-menu'>
+                    <button onClick={() => navigate('/dashboard')} className='nav-link'>
+                        Dashboard
+                    </button>
+                    <button onClick={() => navigate('/profile')} className='nav-link'>Profile</button>
+                    <div className='nav-user'>
+                        <span className="user-email">{user?.email}</span>
+                        <button onClick={handleLogout} className='nav-link Logout'>Logout</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    
+}
